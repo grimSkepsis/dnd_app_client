9c1ee2290a956e5df3fe61d75cf8a20c8b5969f4
grimSkepsis <developer.mikehofer@gmail.com>
2024-11-11 19:40:22 -0800

    grim/gets slightly better currency ui going

--- src/components/inventories/currency/inventory-currency.tsx
+++ src/components/inventories/currency/inventory-currency.tsx
@@ -48,52 +48,72 @@ export function InventoryCurrency({
   }
 
   return (
-    <div>
-      <div className="flex gap-3">
-        <p>Platinum: {pp}</p>
-        <p>Gold: {gp}</p>
-        <p>Silver: {sp}</p>
-        <p>Copper: {cp}</p>
-        <button
-          onClick={onToggleEdit}
-          className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
-        >
-          {isEditing ? (
-            <X className="h-4 w-4" />
-          ) : (
-            <Pencil className="h-4 w-4" />
-          )}
-        </button>
-      </div>
-      {isEditing && (
-        <form {...form} onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex gap-3 max-w-xs">
+    <div className="flex gap-3">
+      <form
+        {...form}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex gap-3"
+      >
+        <div className="flex gap-3 flex-col">
+          <p>Platinum: {pp}</p>
+          {isEditing && (
             <FormField
               control={form.control}
               name="pp"
-              render={({ field }) => <Input type="number" {...field} />}
+              render={({ field }) => (
+                <Input className="w-20" type="number" {...field} />
+              )}
             />
+          )}
+        </div>
+        <div className="flex gap-3 flex-col">
+          <p>Gold: {gp}</p>
+          {isEditing && (
             <FormField
               control={form.control}
               name="gp"
-              render={({ field }) => <Input type="number" {...field} />}
+              render={({ field }) => (
+                <Input className="w-20" type="number" {...field} />
+              )}
             />
+          )}
+        </div>
+        <div className="flex gap-3 flex-col">
+          <p>Silver: {sp}</p>
+          {isEditing && (
             <FormField
               control={form.control}
               name="sp"
-              render={({ field }) => <Input type="number" {...field} />}
+              render={({ field }) => (
+                <Input className="w-20" type="number" {...field} />
+              )}
             />
+          )}
+        </div>
+        <div className="flex gap-3 flex-col">
+          <p>Copper: {cp}</p>
+          {isEditing && (
             <FormField
               control={form.control}
               name="cp"
-              render={({ field }) => <Input type="number" {...field} />}
+              render={({ field }) => (
+                <Input className="w-20" type="number" {...field} />
+              )}
             />
-            <Button variant="outline" size="sm">
-              Save
-            </Button>
-          </div>
-        </form>
-      )}
+          )}
+        </div>
+        {isEditing && (
+          <Button type="submit" size="sm">
+            Save
+          </Button>
+        )}
+      </form>
+      <button
+        onClick={onToggleEdit}
+        className="rounded-sm h-fit opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
+      >
+        {isEditing ? <X className="h-4 w-4" /> : <Pencil className="h-4 w-4" />}
+      </button>
     </div>
   );
 }
